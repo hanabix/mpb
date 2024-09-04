90884bada38712b2a6d6596a9b7b1d555a886240
zhongl <zhong.lunfu@gmail.com>
2024-09-04 14:36:24 +0800

    Clean code.

--- src/garmin/ActivityId.scala
+++ src/garmin/ActivityId.scala
@@ -15,9 +15,8 @@ opaque type ActivityId = String
 object ActivityId:
   def apply(s: String): ActivityId = s
 
-  given by( using Fetch[ActivityId, Intervals], Inject[History]): Inject[List[ActivityId]] = (e, ids) =>
-  for history <- Future.sequence(ids.map(_.request)) if history.nonEmpty do 
-    history.filter(_.nonEmpty).inject(e)
+  given inject(using Fetch[ActivityId, Intervals], Inject[History]): Inject[List[ActivityId]] = (e, ids) =>
+    for history <- Future.sequence(ids.map(_.request)) if history.nonEmpty do history.filter(_.nonEmpty).inject(e)
 
   given fetch(using Fetch[Request, js.Dynamic], Conversion[Get, Request]): Fetch[ActivityId, Intervals] = id =>
     inline def url      = s"https://connect.garmin.cn/activity-service/activity/$id/splits"
@@ -31,6 +30,7 @@ object ActivityId:
       laps = d.asInstanceOf[Splits].lapDTOs.toList
     yield for l <- laps if isActive(l.intensityType)
     yield l.asInstanceOf[Interval]
+    end for
   end fetch
 
   private trait Lap extends js.Object:
