7109f7e4b4dabce5fae57bae546cbaac09d7099f
zhongl <zhong.lunfu@gmail.com>
2024-09-06 19:58:34 +0800

    Fix GMT.

--- src/plotly/DataArrayFrom.scala
+++ src/plotly/DataArrayFrom.scala
@@ -9,7 +9,7 @@ import typings.plotlyJs.mod.PlotType
 import typings.plotlyJs.plotlyJsBooleans.`false`
 import typings.plotlyJs.plotlyJsStrings.y
 
-import core.DateFormat
+import core.DateTimeGMT
 import core.metrics.*
 
 trait DataArrayFrom[A] extends (A => js.Array[Data]):
@@ -17,14 +17,14 @@ trait DataArrayFrom[A] extends (A => js.Array[Data]):
 
 object DataArrayFrom:
 
-  given history(using Performance[Interval], DateFormat[String]): DataArrayFrom[History] = m =>
+  given history(using Performance[Interval], DateTimeGMT[String]): DataArrayFrom[History] = m =>
     inline def box: Intervals => Data = v =>
       Data
         .PartialBoxPlotData()
         .setType(PlotType.box)
         .setY(v.map(_.mpb).toJSArray)
         .setHoverinfo(y)
-        .setName(s"${v.head.startTimeGMT}+08:00".ymd("fr-CA"))
+        .setName(s"${v.head.startTimeGMT}".ymd("fr-CA"))
         .setBoxpoints(`false`)
         .setLine(PartialScatterLine().setWidth(1))
 
--- src/plotly/Title.scala
+++ src/plotly/Title.scala
@@ -1,11 +1,12 @@
 package plotly
 
-import core.DateFormat
+import core.DateTimeGMT
 import core.metrics.*
 
 trait Title[A] extends (A => String):
   extension (a: A) inline def title = this(a)
 object Title:
-  given date(using DateFormat[String]): Title[Intervals] = oi => s"${oi.head.startTimeGMT.ymd("zh-CN")} 速心比走势"
-  given Title[History]                                   = _ => "速心比分布走势"
+  given date(using DateTimeGMT[String]): Title[Intervals] = oi =>
+    s"${(oi.head.startTimeGMT).ymd("fr-CA")} 速心比走势"
+  given Title[History] = _ => "速心比分布走势"
 end Title
