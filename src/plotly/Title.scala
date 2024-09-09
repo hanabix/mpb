97041f6d7e11a4e44e4085c109941578417d5ab8
zhongl <zhong.lunfu@gmail.com>
2024-09-09 15:01:21 +0800

    Retitle.

--- src/plotly/Title.scala
+++ src/plotly/Title.scala
@@ -7,6 +7,6 @@ trait Title[A] extends (A => String):
   extension (a: A) inline def title = this(a)
 object Title:
   given date(using DateTimeGMT[String]): Title[Intervals] = oi =>
-    s"${(oi.head.startTimeGMT).ymd("fr-CA")} 速心比走势"
+    s"${(oi.head.startTimeGMT).ymd("fr-CA")} 速心比关联走势"
   given Title[History] = _ => "速心比分布走势"
 end Title
