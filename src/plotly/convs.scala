984aaab47c69b05aa93f4dcdf09c6da65e8f2558
zhongl <zhong.lunfu@gmail.com>
2024-09-16 17:27:06 +0800

    Clean code.

--- src/plotly/convs.scala
+++ src/plotly/convs.scala
@@ -8,6 +8,5 @@ import org.scalajs.dom.HTMLElement
 import typings.plotlyJs.mod.PlotlyHTMLElement
 
 private[plotly] object convs:
-  inline def accept[A](f: A => Unit): js.Any => Unit  = a => f(a.asInstanceOf[A])
   given Conversion[PlotlyHTMLElement, HTMLElement]    = _.asInstanceOf[HTMLElement]
   given [A]: Conversion[IterableOnce[A], js.Array[A]] = _.toJSArray
